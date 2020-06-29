9a1e44c814a8f9aaac671458d5f98e69f6e70220
cooljasonmelton <cooljasonmelton>
2020-06-29 01:53:43 -0500

    css test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      What up
+    <div className='main-div'>
+      <button> Press Me </button>
+      <div>
+        What up
+      </div>
+      <div class="window mess-with" >
+        <div class="title-bar">
+          <div class="title-bar-text">A Window With Stuff In It</div>
+          <div class="title-bar-controls">
+            <button aria-label="Minimize"></button>
+            <button aria-label="Maximize"></button>
+            <button aria-label="Close"></button>
+        </div>
+      </div>
+        <div class="window-body">
+          <p>COCAINE MURDER JAM</p>
+          <p>cocaine murder jam</p>
+          <p>c o c a</p>
+          <p>i n e m</p>
+          <p>u r d e</p>
+          <p>r j a m</p>
+
+
+
+
+        </div>
+      </div>
 
     </div>
   );
